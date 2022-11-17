61d4c309dfd6299f47bd4126e60cc848fcc2da39
velazmig3200 <82695995+velazmig3200@users.noreply.github.com>
2022-11-17 01:54:29 -0600

    added background image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import cc from "./components/styles/classChain";
 class App extends Component {
 	render() {
 		return (
-			<div className={cc("header", "backgroundImage")}>
+			<div className={cc("header", "topDiv")}>
+				<img
+					className={cc("header", "backgroundImage")}
+					src="https://toppng.com//public/uploads/preview/guitar-strings-neck-guitar-frets-blur-115702405187ynykwgayv.jpg"></img>
 				<Header />
 				<Page Artist="Artist" Song="Song" />
 			</div>
