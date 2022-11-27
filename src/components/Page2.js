788645d33a20e58b054311d5bfb602504076bcd9
velazmig3200 <82695995+velazmig3200@users.noreply.github.com>
2022-11-26 23:54:39 -0600

    moved stray State to top

--- src/components/Page2.js
+++ src/components/Page2.js
@@ -5,13 +5,11 @@ import { musicData } from "./data2.js";
 import cc from "./styles/classChain";
 
 function Page2({ activePage }) {
+	const [value, setValue] = useState("");
 	//default page
 	if (activePage == "") {
 		return (
 			<div className={cc("page", "pageContainer1")}>
-				<p style={{ color: "var(--highlight", paddingTop: "20px" }}>
-					frety is now gluten free!
-				</p>
 				<br />
 				<p>
 					Click on the tabs at the top to search artists, search songs, or view
@@ -22,7 +20,6 @@ function Page2({ activePage }) {
 	}
 
 	//if it's not a song ("artist", "song", or a specific artist)
-	const [value, setValue] = useState("");
 	if (!musicData.list.song("key").includes(activePage)) {
 		return (
 			<div className={cc("page", "pageContainer")}>
